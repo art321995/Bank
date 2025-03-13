833f0113126c42ab931fcde24309e9f76a01abf1
Arturo Esqueda <art270112@gmail.com>
2025-03-13 17:21:41 -0600

    Se realiza el header con animacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import { FirstBlock } from "@/components/FirstBlock";
 import { Header } from "@/components/Header";
 
 
@@ -6,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      
+      <FirstBlock />
     </>
   )
 }
